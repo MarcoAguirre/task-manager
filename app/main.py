6f74b7389b37a0510ee040c0a6a4ced734eeb9d4
MarcoAguirre <viniciosoria16@gmail.com>
2025-06-22 03:36:07 -0500

    feat: Add sample code to create database file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 
+from infrastructure.db.base import Base
+from infrastructure.db.session import engine
+from infrastructure.db import models
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Task Manager API is running"}
+
+Base.metadata.create_all(bind=engine)
