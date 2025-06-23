aea9ed8bffaa49d38412e432cae50d572dc14248
MarcoAguirre <viniciosoria16@gmail.com>
2025-06-23 01:11:44 -0500

    feat: Add task usecases to be able to do a CRUD

---
+++ usecases/task/create.py
@@ -0,0 +1,28 @@
+from uuid import uuid4
+from datetime import datetime
+from domain.models.task import Task, TaskStatus, TaskPriority
+from domain.repositories.task import TaskRepository
+
+
+class CreateTaskUseCase:
+    def __init__(self, repository: TaskRepository):
+        self.repository = repository
+
+    def execute(
+        self,
+        title: str,
+        description: str,
+        priority: TaskPriority,
+        task_list_id: str,
+    ) -> Task:
+        task = Task(
+            id=uuid4(),
+            title=title,
+            description=description,
+            status=TaskStatus.pending,
+            priority=priority,
+            task_list_id=task_list_id,
+            created_at=datetime.utcnow(),
+            updated_at=datetime.utcnow()
+        )
+        return self.repository.create(task)
---
+++ usecases/task/delete.py
@@ -0,0 +1,9 @@
+from domain.repositories.task import TaskRepository
+from uuid import UUID
+
+class DeleteTaskUseCase:
+    def __init__(self, repository: TaskRepository):
+        self.repository = repository
+
+    def execute(self, task_id: UUID) -> None:
+        self.repository.delete(task_id)
