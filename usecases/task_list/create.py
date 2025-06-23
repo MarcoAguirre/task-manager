b906a1d0b300d43ce71b1c0579e280163070fa93
MarcoAguirre <viniciosoria16@gmail.com>
2025-06-22 22:42:51 -0500

    feat: Add create task list usecase

---
+++ usecases/task_list/create.py
@@ -0,0 +1,18 @@
+from domain.models.task_list import TaskList
+from domain.repositories.task_list import TaskListRepository
+from uuid import uuid4
+from datetime import datetime
+
+class CreateTaskListUseCase:
+    def __init__(self, repository: TaskListRepository):
+        self.repository = repository
+
+    def execute(self, name: str, description: str = "") -> TaskList:
+        task_list = TaskList(
+            id=uuid4(),
+            name=name,
+            description=description,
+            created_at=datetime.now(),
+            updated_at=datetime.now()
+        )
+        return self.repository.create(task_list)
